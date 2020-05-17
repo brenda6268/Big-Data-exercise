a4f370f8e88ea3fc1846dc9b25b1e424f8ce1bde
satya <38804803+satyamuralidhar@users.noreply.github.com>
2020-05-17 23:55:46 +0530

    Update No32-Loading-spam-and-non-spam-data.py

--- big-data--fundamentals-pyspark/No32-Loading-spam-and-non-spam-data.py
+++ big-data--fundamentals-pyspark/No32-Loading-spam-and-non-spam-data.py
@@ -23,8 +23,8 @@ spam_rdd = sc.textFile(file_path_spam)
 non_spam_rdd = sc.textFile(file_path_non_spam)
 
 # Split the email messages into words
-spam_words = spam_rdd.map(lambda email: email.split(' '))
-non_spam_words = non_spam_rdd.map(lambda email: email.split(' '))
+spam_words = spam_rdd.flatMap(lambda email: email.split(' '))
+non_spam_words = non_spam_rdd.flatMap(lambda email: email.split(' '))
 
 # Print the first element in the split RDD
 print("The first element in spam_words is", spam_words.first())
