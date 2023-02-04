13247544c4cca30447cae70201c380605577c46c
Maxim K <111058027+Maximkoblyukwork@users.noreply.github.com>
2023-02-04 16:41:31 +0100

    Update No16-Print-word-frequencies.py

--- big-data--fundamentals-pyspark/No16-Print-word-frequencies.py
+++ big-data--fundamentals-pyspark/No16-Print-word-frequencies.py
@@ -16,19 +16,19 @@ Print the top 10 most frequent words and their frequencies.
 '''
 
 # Code
-# Display the first 10 words and their frequencies
+# Display the first 10 words and their frequencies from the input RDD
 for word in resultRDD.take(10):
 	print(word)
 
-# Swap the keys and values 
+# Swap the keys and values from the input RDD
 resultRDD_swap = resultRDD.map(lambda x: (x[1], x[0]))
 
 # Sort the keys in descending order
 resultRDD_swap_sort = resultRDD_swap.sortByKey(ascending=False)
 
-# Show the top 10 most frequent words and their frequencies
+# Show the top 10 most frequent words and their frequencies from the sorted RDD
 for word in resultRDD_swap_sort.take(10):
-	print("{} has {} counts". format(word[1], word[0]))
+	print("{},{}". format(word[1], word[0]))
 
 
 '''
